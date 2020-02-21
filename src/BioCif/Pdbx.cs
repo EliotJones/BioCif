cf4b12dde297ce51105cb0c40fa84ae687faac5e
Eliot Jones <elioty@hotmail.co.uk>
2020-02-21 12:15:15 +0000

    add parser and container type for the pdbx parser
    the pdbx parser will wrap the cifparser to return a data structure interpreted according to the pdbx dictionary from the underlying cif file.

---
+++ src/BioCif/Pdbx.cs
@@ -0,0 +1,29 @@
+﻿namespace BioCif
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Data from a macro-molecular CIF / PDBX Protein Data Bank file.
+    /// </summary>
+    public class Pdbx
+    {
+        /// <summary>
+        /// The first <see cref="PdbxDataBlock"/> in the file.
+        /// </summary>
+        public PdbxDataBlock First => DataBlocks?.Count > 0 ? DataBlocks[0] : null;
+
+        /// <summary>
+        /// The set of <see cref="PdbxDataBlock"/>s in the file.
+        /// </summary>
+        public IReadOnlyList<PdbxDataBlock> DataBlocks { get; }
+
+        /// <summary>
+        /// Create a new <see cref="Pdbx"/>.
+        /// </summary>
+        public Pdbx(IReadOnlyList<PdbxDataBlock> dataBlocks)
+        {
+            DataBlocks = dataBlocks ?? throw new ArgumentNullException(nameof(dataBlocks));
+        }
+    }
+}
---
+++ src/BioCif/PdbxParser.cs
@@ -0,0 +1,41 @@
+﻿namespace BioCif
+{
+    using System.IO;
+    using System.Text;
+    using Core;
+    using Core.Parsing;
+    using Version = Core.Version;
+
+    /// <summary>
+    /// Parse a <see cref="Pdbx"/> from a CIF file.
+    /// </summary>
+    public static class PdbxParser
+    {
+        public static Pdbx ParseString(string cif)
+        {
+            using (var stream = new MemoryStream(Encoding.Unicode.GetBytes(cif)))
+            {
+                return ParseStream(stream, Encoding.Unicode);
+            }
+        }
+
+        public static Pdbx ParseFile(string filePath, Encoding encoding = null)
+        {
+            using (var fs = File.OpenRead(filePath))
+            {
+                return ParseStream(fs, encoding);
+            }
+        }
+        
+        public static Pdbx ParseStream(Stream stream, Encoding encoding = null)
+        {
+            var cif = CifParser.Parse(stream, new CifParsingOptions
+            {
+                Version = Version.Version2,
+                FileEncoding = encoding
+            });
+
+            return null;
+        }
+    }
+}
